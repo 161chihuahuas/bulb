32e8592f278342620cf8ee85884580ba17f9f851
Lily Anne <lily.anne.hall@protonmail.com>
2025-05-29 22:10:10 -0700

    functional e2e test

--- test/context.e2e.js
+++ test/context.e2e.js
@@ -1,112 +1,36 @@
 'use strict';
 
 const { expect } = require('chai');
-const proxyquire = require('proxyquire');
-const sinon = require('sinon');
-const { EventEmitter } = require('events');
-const { TorController } = require('..');
+const { TorContext } = require('..');
 
 
-describe('@module granax', function() {
+describe('@class TorContext', function() {
 
-  describe('@exports', function() {
+  this.timeout(120000); // Depending on where this is running it could take a while to bootstrap
 
-    this.timeout(10000);
+  it('@static create()', function() {
+    return new Promise(async (resolve, reject) => {
+      const tor = await TorContext.create();
+      const server = tor.createServer(echoAndClose); 
 
-    let sandbox = sinon.createSandbox();
-    let _execFileSync = sandbox.stub();
-    let _proc = new EventEmitter();
-    _proc.stdout = new EventEmitter();
-    _proc.kill = sandbox.stub();
-    let _spawn = sandbox.stub().returns(_proc);
-    let _socket = new EventEmitter();
-    _socket.connect = sandbox.stub();
-    _socket.pipe = (o) => o;
-    function _Socket() {
-      return _socket;
-    }
-    let _readFileSync = sandbox.stub().returns('127.0.0.1:9051');
-    let granax = proxyquire('..', {
-      'node:child_process': {
-        execFileSync: _execFileSync,
-        spawn: _spawn
-      },
-      net: {
-        Socket: _Socket
-      },
-      fs: {
-        readFileSync: _readFileSync
-      }
+      function echoAndClose(socket) {
+        socket.once('data', (data) => {
+          socket.end(data);
+        });
+      } 
+
+      const address = await server.listen();
+      const socket = tor.createConnection();
+
+      await socket.connect(address);
+
+      socket.once('data', data => {
+        expect(data.toString()).to.equal('hello');
+        resolve();
+      });
+
+      socket.write('hello');
     });
-    let tor = null;
-
-    before(() => {
-      tor = granax();
-    });
-    after(() => sandbox.restore());
-
-    it('should return a TorController', function() {
-      expect(tor).to.be.instanceOf(TorController);
-    });
-
-    it('should connect on stdout from child', function(done) {
-      _proc.stdout.emit('data', 'tor is running');
-      setTimeout(() => {
-        expect(_socket.connect.called).to.equal(true);
-        done();
-      }, 1000);
-    });
-
-    it('should bubble child error on controller', function(done) {
-      tor.once('error', () => done());
-      setImmediate(() => _proc.emit('error', new Error('tor failed')));
-    });
-
-  });
-
-  describe('@function tor', function() {
-
-    let sandbox = sinon.createSandbox();
-    let _execFileSync = sandbox.stub().returns('/usr/bin/tor');
-    let granax = proxyquire('..', {
-      child_process: {
-        execFileSync: _execFileSync
-      }
-    });
-
-    after(() => sandbox.restore());
-
-    it.skip('should return the windows path', function() {
-      expect(granax.tor('win32').includes(
-        'bin/Browser/TorBrowser/Tor/tor.exe'
-      )).to.equal(true);
-    });
-
-    it.skip('should return the macintosh path', function() {
-      expect(granax.tor('darwin').includes(
-        'bin/.tbb.app/Contents/MacOS/Tor/tor.real'
-      )).to.equal(true);
-    });
-
-    it.skip('should return the gnu+linux path', function() {
-      expect(granax.tor('linux').includes(
-        'bin/tor-browser_en-US/Browser/TorBrowser/Tor/tor'
-      )).to.equal(true);
-      expect(granax.tor('android').includes(
-        'bin/tor-browser_en-US/Browser/TorBrowser/Tor/tor'
-      )).to.equal(true);
-    });
-
-    it('should throw if tor not installed on gnu_linux', function() {
-      expect(function() {
-        granax.tor('blackberry lol');
-      }).to.throw(Error, 'Unsupported platform "blackberry lol"');
-    });
-
-    it('should throw if unsupported platform', function() {
-
-    });
-
   });
 
 });
